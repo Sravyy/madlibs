d604c6b2eb0fc5ceebce5cb8458a8c6b27b1f5f9
saneyee sarkar and sravanthi velaga <sravanthi@>
2017-09-18 11:55:56 -0700

    Add for loop in JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
   $("#formOne").submit(function(event){
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var person3Input = $("input#person3").val();
-    var person4Input = $("input#person4").val();
+    // var person1Input = $("input#person1").val();
+    // var person2Input = $("input#person2").val();
+    // var person3Input = $("input#person3").val();
+    // var person4Input = $("input#person4").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".person3").text(person3Input);
-    $(".person4").text(person4Input);
+    var inputs = ["person1", "person2", "person3", "person4"];
+    inputs.forEach(function(input){
+      var userInput = $("input#"+ input).val();
+      $("."+ input).text(userInput);
+    });
+
+    // $(".person1").text(person1Input);
+    // $(".person2").text(person2Input);
+    // $(".person3").text(person3Input);
+    // $(".person4").text(person4Input);
 
     $("#story").show();
 
